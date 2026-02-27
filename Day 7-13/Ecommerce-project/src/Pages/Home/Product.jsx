6f2d7ff4b974a5e1a2bf13d41899459825ec7cf9
yousmakhayyam <yousmakhayyam@gmail.com>
2026-02-27 12:11:29 +0500

    completed project

--- Day 7-13/Ecommerce-project/src/Pages/Home/Product.jsx
+++ Day 7-13/Ecommerce-project/src/Pages/Home/Product.jsx
@@ -18,7 +18,8 @@ export function Product({ product, loadCart }) {
     };
 
     return (
-        <div className="product-container">
+        <div className="product-container"
+        data-testid = "product-container">
             <div className="product-image-container">
                 <img
                     className="product-image"
